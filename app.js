67226d365672087c945f24e09a4661dbb1912382
Harry Gothold <your.name@kurtgeiger.comgit config --global user.email harry.gothold@kurtgeiger.com>
2018-11-01 17:58:23 +0000

    Adding existing files

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+// const expressHbs = require('express-handlebars');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.set('view engine', 'pug');
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+// app.set('view engine', 'hbs');
+// app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminData.routes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
-});
+app.use(errorController.get404);
 
 //.use - allows to add new middleware function
 //next is function which is passed to the use method callback - has to be used to allow request to passed onto next middleware
