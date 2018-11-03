f2cd6627c555614475229334cf94ad722a581546
Harry Gothold <harrygothold@harrys-mbp.home>
2018-11-03 20:25:31 +0000

    Added new cart feature

--- controllers/shop.js
+++ controllers/shop.js
@@ -49,7 +49,7 @@ exports.getCart = (req,res,next) => {
             res.render('shop/cart', {
                 path: '/cart',
                 pageTitle: 'Your Cart',
-                products: cartProducts
+                products: products
         })
     })
         .catch(err => console.log(err))
@@ -59,15 +59,42 @@ exports.getCart = (req,res,next) => {
 
 exports.postCart = (req,res,next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, (product) => {
-        Cart.addProduct(prodId, product.price);
-    });
-    res.redirect('/cart');
+    let fetchedCart;
+    let newQuantity = 1;
+    req.user
+    .getCart()
+    .then(cart => {
+        fetchedCart = cart;
+        return cart.getProducts({where: {id: prodId}});
+    })
+    .then(products => {
+        let product;
+        if (products.length > 0) {
+            product = products[0];
+        }
+        if (product) {
+            const oldQuantity = product.cartItem.quantity;
+            newQuantity = oldQuantity + 1;
+            return product;
+
+        }
+        return Product.findByPk(prodId)
+    })
+    .then(product => {
+        return fetchedCart.addProduct(product, {
+            through: {quantity: newQuantity}
+        });
+    })
+    .catch(err => console.log(err))
+    .then(() => {
+        res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCarDeleteProduct = (req,res,next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, product => {
+    Product.findByPk(prodId, product => {
         Cart.deleteProduct(prodId, product.price);
         res.redirect('/cart');
     });
