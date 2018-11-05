9d7632845a65bb636a9da6214cd3819ce5eae40d
Harry Gothold <your.name@kurtgeiger.comgit config --global user.email harry.gothold@kurtgeiger.com>
2018-11-05 17:17:30 +0000

    switched sql features to NoSql

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,13 +20,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req,res,next) => {
-    // User.findById(1)
-    // .then(user => {
-    //     req.user = user;
-    //     next();
-    // })
-    // .catch(err => console.log(err));
-    next();
+    User.findById('5be0671937c93130d9046345')
+    .then(user => {
+        req.user = new User(user.name, user.email, user.cart, user._id);
+        next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin',adminRoutes);
--- models/product.js
+++ models/product.js
@@ -3,16 +3,27 @@ const getDb = require('../util/database').getDb;
 
 
 class Product {
-    constructor(title, price, description, imageUrl) {
+    constructor(title, price, description, imageUrl, id, userId) {
         this.title = title;
         this.price = price;
         this.description = description;
         this.imageUrl = imageUrl;
+        this._id = id;
+        this.userId = userId;
     }
 
     save() {
         const db = getDb();
-        return db.collection('products').insertOne(this)
+        let dbOp;
+        if (this._id) {
+            // Update
+            dbOp = db.collection('products').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: this});
+        } else {
+            dbOp = db.collection('products')
+            .insertOne(this)
+
+        }
+        return dbOp
         .then(result => {
             console.log(result);
         })
@@ -34,11 +45,20 @@ class Product {
     static findById(prodId) {
         const db = getDb();
         return db.collection('products')
-        .find({_id: new mongodb.ObjectID(prodId)})
+        .find({_id: new mongodb.ObjectId(prodId)})
         .next()
         .then(product => {
             console.log(product);
             return product;
+        })
+        .catch(err => console.log(err));
+    }
+
+    static deleteById(prodId) {
+        const db = getDb();
+        return db.collection('products').deleteOne({_id: new mongodb.ObjectId(prodId)})
+        .then(result => {
+            console.log('Deleted');
         })
         .catch(err => console.log(err));
     }
